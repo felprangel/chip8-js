5a4136009045c3d37a028f0316e9bfa5707f7214
felprangel <feliperangel897@gmail.com>
2026-01-20 12:58:30 -0300

    ajusta tamanho da stack

--- chip8.js
+++ chip8.js
@@ -3,8 +3,8 @@ class Chip8 {
     this.ram = new Uint8Array(4096);
     this.V = new Uint8Array(16);
     this.I = 0;
-    this.stack = new Uint16Array(16);
     this.PC = 0x200; // entrypoint for memory
+    this.stack = new Uint16Array(12);
     this.stackPointer = 0;
     this.delayTimer = 0;
     this.soundTimer = 0;
