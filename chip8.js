b9bf27811ac2786050b81b6876763efc4528a253
felprangel <feliperangel897@gmail.com>
2026-01-20 12:58:17 -0300

    adiciona comentario

--- chip8.js
+++ chip8.js
@@ -3,8 +3,8 @@ class Chip8 {
     this.memory = new Uint8Array(4096);
     this.V = new Uint8Array(16);
     this.I = 0;
-    this.PC = 0x200;
     this.stack = new Uint16Array(16);
+    this.PC = 0x200; // entrypoint for memory
     this.stackPointer = 0;
     this.delayTimer = 0;
     this.soundTimer = 0;
