bd4c8f140e56137b5133d42f3e90b1f283aad722
felprangel <feliperangel897@gmail.com>
2026-01-20 17:17:36 -0300

    ajusta entrypoint

--- chip8.js
+++ chip8.js
@@ -1,9 +1,11 @@
 class Chip8 {
+  entrypoint = 0x200;
+
   constructor() {
     this.ram = new Uint8Array(4096);
     this.V = new Uint8Array(16);
     this.I = 0;
-    this.PC = 0x200; // entrypoint for memory
+    this.PC = this.entrypoint;
     this.stack = new Uint16Array(12);
     this.stackPointer = 0;
     this.delayTimer = 0;
