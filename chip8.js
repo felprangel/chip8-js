de1d413bac7f5c03dbe574e505df0c9989ef4790
felprangel <feliperangel897@gmail.com>
2026-01-20 17:17:56 -0300

    adiciona logica para carregar fonte e rom na memoria

--- chip8.js
+++ chip8.js
@@ -42,4 +42,118 @@ class Chip8 {
         break;
     }
   }
+
+  init() {
+    this.loadFont();
+    this.loadROM();
+  }
+
+  loadFont() {
+    const font = new Uint8Array([
+      0xf0,
+      0x90,
+      0x90,
+      0x90,
+      0xf0, // 0
+      0x20,
+      0x60,
+      0x20,
+      0x20,
+      0x70, // 1
+      0xf0,
+      0x10,
+      0xf0,
+      0x80,
+      0xf0, // 2
+      0xf0,
+      0x10,
+      0xf0,
+      0x10,
+      0xf0, // 3
+      0x90,
+      0x90,
+      0xf0,
+      0x10,
+      0x10, // 4
+      0xf0,
+      0x80,
+      0xf0,
+      0x10,
+      0xf0, // 5
+      0xf0,
+      0x80,
+      0xf0,
+      0x90,
+      0xf0, // 6
+      0xf0,
+      0x10,
+      0x20,
+      0x40,
+      0x40, // 7
+      0xf0,
+      0x90,
+      0xf0,
+      0x90,
+      0xf0, // 8
+      0xf0,
+      0x90,
+      0xf0,
+      0x10,
+      0xf0, // 9
+      0xf0,
+      0x90,
+      0xf0,
+      0x90,
+      0x90, // A
+      0xe0,
+      0x90,
+      0xe0,
+      0x90,
+      0xe0, // B
+      0xf0,
+      0x80,
+      0x80,
+      0x80,
+      0xf0, // C
+      0xe0,
+      0x90,
+      0x90,
+      0x90,
+      0xe0, // D
+      0xf0,
+      0x80,
+      0xf0,
+      0x80,
+      0xf0, // E
+      0xf0,
+      0x80,
+      0xf0,
+      0x80,
+      0x80, // F
+    ]);
+
+    this.ram.set(font);
+  }
+
+  loadROM() {
+    const input = document.querySelector("#romInput");
+
+    input.addEventListener("change", function (event) {
+      const file = event.target.files[0];
+
+      if (!file) {
+        return;
+      }
+
+      const reader = new FileReader();
+
+      reader.onload = function (event) {
+        const buffer = event.target.result;
+        const bytes = new Uint8Array(buffer);
+        this.ram.set(bytes, this.entrypoint);
+      };
+
+      reader.readAsArrayBuffer(file);
+    });
+  }
 }
