ab7e16d9513c4d9ab4c3503e7c470598eb7168be
felprangel <feliperangel897@gmail.com>
2026-01-20 12:58:43 -0300

    adiciona funcao inicial de ciclo da cpu

--- chip8.js
+++ chip8.js
@@ -9,4 +9,34 @@ class Chip8 {
     this.delayTimer = 0;
     this.soundTimer = 0;
   }
+
+  cpuCycle() {
+    const opcode = (this.ram[this.PC] << 8) | this.ram[this.PC + 1];
+    this.PC += 2;
+
+    const NNN = opcode & 0x0fff;
+    const NN = opcode & 0x0ff;
+    const N = opcode & 0x0f;
+    const X = (opcode >> 8) & 0x0f;
+    const Y = (opcode >> 4) & 0x0f;
+
+    const type = (opcode >> 12) & 0x0f;
+
+    switch (type) {
+      case 0x01:
+        this.PC = NNN;
+        break;
+
+      case 0x06:
+        this.V[X] = NN;
+        break;
+
+      case 0x07:
+        this.V[X] += NN;
+        break;
+
+      default:
+        break;
+    }
+  }
 }
